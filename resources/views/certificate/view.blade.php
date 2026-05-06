df5a27d7399b6c3e6253b8ff9cec9df46cdcb957
zainab-dev1 <zainab.alawaid1@gmail.com>
2026-05-06 22:36:55 +0400

    up

--- resources/views/certificate/view.blade.php
+++ resources/views/certificate/view.blade.php
@@ -127,12 +127,12 @@
         @if($fromDate)
             @if($isMultiDay)
                 <p class="cert_date">As part of the 1<sup>st</sup> Academic Creativity Week - ضمن فعاليات أسبوع الإبداع الأكاديمي الأول </p>
-                <p class="cert_date" style="dir:rtl;">التي اقيمـت من {{ $fromAr }} إلى {{ $toAr }} بفـرع الجامعة </p>
-                <p class="cert_date">Held from {{ $fromEn }} to {{ $toEn }} at UTAS-Salalah</p>
+                <p class="cert_date" style="dir:rtl; margin-top: -1px;">التي اقيمـت من {{ $fromAr }} إلى {{ $toAr }} بفـرع الجامعة </p>
+                <p class="cert_date" style="margin-top: -1px;">Held from {{ $fromEn }} to {{ $toEn }} at UTAS-Salalah</p>
             @else
                 <p class="cert_date">As part of the 1<sup>st</sup> Academic Creativity Week - ضمن فعاليات أسبوع الإبداع الأكاديمي الأول </p>
-                <p class="cert_date" style="dir:rtl;">التي اقيمـت في {{ $fromAr }} بفـرع الجامعة </p>
-                <p class="cert_date">Held on {{ $fromEn }} at UTAS-Salalah</p>
+                <p class="cert_date" style="dir:rtl; margin-top: -1px;">التي اقيمـت في {{ $fromAr }} بفـرع الجامعة </p>
+                <p class="cert_date" style="margin-top: -1px;">Held on {{ $fromEn }} at UTAS-Salalah</p>
             @endif
         @endif
         <br>
