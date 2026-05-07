c54cf27cab823ec9306e80d51c87756e0d248929
zainab-dev1 <zainab.alawaid1@gmail.com>
2026-05-07 15:04:13 +0400

    up

--- resources/views/certificate/view.blade.php
+++ resources/views/certificate/view.blade.php
@@ -149,10 +149,10 @@
         @if($fromDate)
             @if($isMultiDay)
                 <p class="cert_date">ضمن فعاليات أسبوع الإبداع الأكاديمي الأول </p>
-                <p class="cert_date">التي اقيمـت في ١١-١٣ مايو٢٠٢٦ بفرع الجامعة</p>
+                <p class="cert_date cert_date_ar" dir="rtl">التي اقيمـت في ١١ - ١٣ مايو بفرع الجامعة</p>
             @else
                 <p class="cert_date">ضمن فعاليات أسبوع الإبداع الأكاديمي الأول </p>
-                <p class="cert_date">التي اقيمـت في ١١-١٣ مايو٢٠٢٦ بفرع الجامعة</p>
+                <p class="cert_date cert_date_ar" dir="rtl">التي اقيمـت في ١١ - ١٣ مايو بفرع الجامعة</p>
             @endif
         @endif
         <br><br>
